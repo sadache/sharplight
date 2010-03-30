1a6fe50214d7b83c224936eb5f97374eccd43a09
Sadek Drobi <github@sadekdrobi.com>
2010-03-31 00:27:56 +0200

    Tiny improvements

--- SharpLight/SimpleDispatcher.fs
+++ SharpLight/SimpleDispatcher.fs
@@ -10,7 +10,7 @@ let swap f = fun b  a-> f a b
 
 type Servlet=  Request->Response
 and Url= String
-and Request= {Request:Web.HttpRequest;UrlParts:string list;Method:Method}
+and Request= {Request:Web.HttpRequest;UrlParts:string LazyList;Method:Method;AcceptedMime: string seq}
 and Method= Get|Post|Put|Unsupported
 and Response= Web.HttpResponse -> unit
 type Matcher = abstract member Do: (Request->  Servlet Option)
@@ -18,16 +18,19 @@ type Matcher = abstract member Do: (Request->  Servlet Option)
 
 and 'a ParamMatcher='a->Matcher
 
+let matchIt rq (matchers:Matcher list)=  [for m in matchers -> m.Do]|> Seq.tryPick ((|>) rq) 
+
 [<AbstractClass>]
 type LightController() = 
            abstract member Matchers : Matcher list
            interface  System.Web.IHttpHandler with
                 member x.ProcessRequest context= 
                                 let makeRequest r=   {Request=r; 
-                                                      UrlParts= (r.Path.Trim [|'/'|]) .Split [|'/'|] |> Seq.toList;
-                                                      Method=match r.HttpMethod with |"GET"->Get |"Post" ->Post|_-> Unsupported}
+                                                      UrlParts= (r.Path.Trim [|'/'|]) .Split [|'/'|] |> LazyList.ofArray
+                                                      Method=match r.HttpMethod with |"GET"->Get |"Post" ->Post|_-> Unsupported;
+                                                      AcceptedMime=r.AcceptTypes}
                                 let request=makeRequest context.Request 
-                                let   matched=[for m in x.Matchers -> m.Do]|> Seq.tryPick ((|>) request)                                                           
+                                let   matched=matchIt request x.Matchers                                                          
                                 in match matched with
                                                |None-> context.Response.StatusCode <- 404
                                                |Some servlet -> servlet request context.Response
@@ -35,35 +38,34 @@ type LightController() =
 ///Need to test how effecient this function is
 ///
 
+
 let splitAt n xs=(LazyList.take n xs,LazyList.skip n xs)
-//need to include the case of dir as already including two levels
+
 let dir (dir:string)(subs :Matcher  list) : Matcher=  
-           {new Matcher with
-             member x.Do=fun rq->let dirSplitted=(LazyList.ofArray <| dir.Split('/'))
-                                 let concerned,rest= splitAt (LazyList.length dirSplitted) (LazyList.ofList rq.UrlParts)
-                                 in if  dirSplitted = concerned then [for m in subs -> m.Do] |> Seq.tryPick ((|>) {rq with UrlParts=LazyList.toList rest}) 
+           let dirSplitted=(LazyList.ofArray <| dir.Split('/'))
+           in {new Matcher with
+             member x.Do=fun rq->let concerned,rest= splitAt (LazyList.length dirSplitted) rq.UrlParts
+                                 in if  dirSplitted = concerned then matchIt  {rq with UrlParts= rest} subs 
                                     else None
              member x.Info= "not documented"  }
 
 let any (s : Servlet) :Matcher= {new Matcher with member x.Do= fun _->Some s
                                                   member x.Info= "Any"}
 
-let mime_types ms s:Matcher= {new Matcher with member x.Do=fun rq-> let doesMatch=  Seq.exists2  (=)  ms rq.Request.AcceptTypes 
-                                                                    in if doesMatch then Some s else None
-                                               member x.Info= String.concat " + " ms}
+let mime_types ms s:Matcher= {new Matcher with 
+                                     member x.Do=fun rq-> let doesMatch=  Seq.exists2  (=)  ms rq.AcceptedMime 
+                                                          in if doesMatch then Some s else None
+                                     member x.Info= String.concat " + " ms}
 
-let webMethod m s : Matcher= {new Matcher with member x.Do =fun rq -> if rq.Method = m then Some s else None
-                                               member x.Info= m.ToString()}
-
+let webMethod m s : Matcher= {new Matcher with 
+                                     member x.Do =fun rq -> if rq.Method = m then Some s else None
+                                     member x.Info= m.ToString() }
 let web_get s= webMethod Get s
 let web_post s= webMethod Post s
 
-let webMethod1 m (subs :Matcher  list) ={new Matcher with member x.Do = fun(rq) -> if rq.Method = m then  [for m in subs -> m.Do]  |> Seq.tryPick ((|>) rq)
-                                                                                    else None
-                                                           member x.Info= m.ToString()}
-
-
-
+let webMethod1 m (subs :Matcher  list) ={new Matcher with
+                                                member x.Do = fun(rq) -> if rq.Method = m then matchIt rq subs else None
+                                                member x.Info= m.ToString()}
 
 let lift2 f  =fun b c -> fun a -> f (b a) (c a)   
 let lift1 f= fun g a -> f(g(a))
@@ -79,8 +81,8 @@ let (-|) s1 s2:Servlet= s1 +^^ s2
 let (|=)= (<|)
 let (=>)= (<|)
 
-let yield_string   (s:string):Servlet= fun _ -> fun r -> do r.Output.Write s 
-                                                         r.Flush()
+let yield_string (s:string):Servlet= fun _ -> fun r -> do r.Output.Write s 
+                                                          r.Flush()
                                                          
 let mime t :Servlet = fun _ -> fun r -> r.ContentType <- t
 let code code :Servlet=fun _ -> fun r -> r.StatusCode  <- code
