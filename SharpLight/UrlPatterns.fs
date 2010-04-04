a57174fc8bd80ba9bcff1163067893bb6223f373
Sadek Drobi <github@sadekdrobi.com>
2010-04-04 21:14:06 +0200

    UrlPatterns

---
+++ SharpLight/UrlPatterns.fs
@@ -0,0 +1,15 @@
+﻿module UrlPatterns
+open System.Text.RegularExpressions
+let matchUrl pattern:string->Map<string,string> Option=
+            let regex=(new Regex(pattern))
+            let names= regex. GetGroupNames()
+            in fun url -> let m=regex.Match(url)
+                          if not(m.Success) then None
+                          else let groups= m.Groups in
+                               [for i in 0..groups.Count do 
+                                let key= regex.GroupNameFromNumber(i)
+                                if not <| System.String.IsNullOrEmpty key 
+                                then yield (regex.GroupNameFromNumber(i), groups.[i].Value)] |> Map.ofList |> Some
+
+let urlParams= matchUrl "Show/(?<topics>.*)/(?<id>[\\d]+)/(?<title>.*)" "http://localhost/Show/Topic/SubTopic/SubSubTopic/123/This-is-an-example"
+
